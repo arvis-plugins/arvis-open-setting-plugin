3623a3c4be770c8e6c097d62635ec1859b0548a6
jopemachine <jopemachine@naver.com>
2021-08-28 03:13:25 +0900

    Upd

--- index.js
+++ index.js
@@ -1,10 +1,12 @@
+const arvish = require('arvish');
+
 module.exports = ({ inputStr }) => {
   const arg = process.platform === 'darwin' ? 'x-apple.systempreferences:com.apple.preference' : 'ms-settings:';
 
   return {
     items: [
       {
-        title: 'Open setting',
+        title: arvish.env.get('string', 'command'),
         arg
       }
     ],
